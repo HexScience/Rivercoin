40a795e57c8fb442d06a60c287026fe697b74299
Riverssen <RagnarrIvarssen@gmail.com>
2018-06-12 20:53:52 +0200

    Updated New Keyword

--- src/main/java/com/riverssen/core/mpp/compiler/ParsedProgram.java
+++ src/main/java/com/riverssen/core/mpp/compiler/ParsedProgram.java
@@ -279,9 +279,9 @@ public class ParsedProgram
 
         if (neW.getTokens().get(0).getType() != Token.Type.METHOD_CALL && neW.getTokens().get(0).getType() != Token.Type.IDENTIFIER) throw new ParseException("new must be followed by a function or typename " + neW, neW);
 
-        neW.getTokens().get(0).setType(Token.Type.NEW);
+//        neW.getTokens().get(0).setType(Token.Type.NEW);
 
-        rootm.add(neW.getTokens().get(0));
+        rootm.add(neW);
     }
 
     private void parseKeyword(List<Token> tokens, Token root) throws ParseException
