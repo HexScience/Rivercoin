06f9b057902b52462a922975f7f3cf4d6c5792a9
Riverssen <RagnarrIvarssen@gmail.com>
2018-07-21 04:50:29 +0200

    .

--- core/network.h
+++ core/network.h
@@ -2,8 +2,8 @@
 //// Created by Ragnarr Ivarssen on 09.07.18.
 ////
 //
-//#ifndef RIVERCOIN_CPP_NETWORK_H
-//#define RIVERCOIN_CPP_NETWORK_H
+#ifndef RIVERCOIN_CPP_NETWORK_H
+#define RIVERCOIN_CPP_NETWORK_H
 //
 //#include <cstdlib>
 //#include <iostream>
@@ -29,4 +29,31 @@
 //    DataPacket    packet;
 //};
 //
-//#endif //RIVERCOIN_CPP_NETWORK_H
+
+#include "serializeable.h"
+
+#define MSG_RECEIVED_SUCCESSFULLY 0
+#define MSG_CORRUPTED 1
+#define MSG_BLOCK_SOLUTION 2
+#define MSG_FULL_BLOCK_GET 3
+#define MSG_TRANSACTION 4
+#define MSG_BLOCKCHAIN_SIZE 5
+#define MSG_GREET 6
+#define MSG_GREET_BACK 7
+#define MSG_GOODBYE 8
+#define MSG_FULL_BLOCK_SET 9
+
+
+struct Message{
+    serializeable<char*> s;
+};
+
+class Client{
+private:
+public:
+    void send(Message msg);
+    void connect();
+    void disconnect();
+};
+
+#endif //RIVERCOIN_CPP_NETWORK_H
