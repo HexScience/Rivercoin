95e64b5be491d5d1ccce09d0f8892c5e7ad39b41
Riverssen <RagnarrIvarssen@gmail.com>
2018-07-30 01:47:26 +0200

    .

--- core/network.cpp
+++ core/network.cpp
@@ -1,50 +1,62 @@
-////
-//// Created by Ragnarr Ivarssen on 09.07.18.
-////
 //
-//#include "network.h"
-//#include "context.h"
-//#include "config.h"
+// Created by Ragnarr Ivarssen on 09.07.18.
 //
-//#include <boost/asio.hpp>
-//#include <boost/array.hpp>
-//#include <iostream>
-//
-//SocketConnection::SocketConnection(const std::string _ip_, unsigned short _port_) : ip(_ip_), port(_port_), socket(ios)
-//{
-//}
-//
-//void SocketConnection::makeConnection()
-//{
-//    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
-//
-//    socket.connect(endpoint);
-//}
-//
-//bool SocketConnection::isConnected()
-//{
-//    return socket.is_open();
-//}
-//
-//void SocketConnection::disconnect()
-//{
-//    socket.close();
-//}
-//
-//void SocketConnection::send(const std::string& msg, boost::system::error_code& error)
-//{
-////    boost::array<char, 4> buf;
-////    std::copy(msg.begin(), msg.end(), buf);
-//
-////    boost::system::error_code e;
-////    (&error)[0] = e;
-//
-////    socket.async_write_some(boost::asio::buffer(buf, msg.size()), error);
-//}
-//
-//void SocketConnection::receive()
-//{
-////    socket.read_some(out);
-//}
-//
-//
+
+#include "network.h"
+#include "context.h"
+#include "config.h"
+
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+
+#ifndef SOCKETS_
+#define SOCKETS_
+
+struct Socket{
+    boost::asio::io_service                 ios;
+    boost::asio::ip::tcp::socket            socket;
+
+    Socket() : socket(ios) {}
+//    Socket(const Socket& o) : socket(o.socket) { ios = socket.get_io_context(); }
+};
+
+struct ServerSocket{
+};
+
+#endif
+
+SocketConnection::SocketConnection(const std::string _ip_, unsigned short _port_) : ip(_ip_), port(_port_)
+{
+}
+
+void SocketConnection::makeConnection()
+{
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
+
+    socket->socket.connect(endpoint);
+}
+
+bool SocketConnection::isConnected()
+{
+    return socket->socket.is_open();
+}
+
+void SocketConnection::disconnect()
+{
+    socket->socket.close();
+}
+
+void SocketConnection::send(const std::string& msg)
+{
+    boost::system::error_code e;
+
+//    socket->socket.async_write_some(boost::asio::buffer(msg.c_str(), msg.size()), e);
+}
+
+void SocketConnection::receive()
+{
+//    socket->socket.read_some(out);
+}
+
+
--- core/network.h
+++ core/network.h
@@ -1,101 +1,98 @@
-//////
-////// Created by Ragnarr Ivarssen on 09.07.18.
-//////
 ////
-//#ifndef RIVERCOIN_CPP_NETWORK_H
-//#define RIVERCOIN_CPP_NETWORK_H
-////
-////#include <cstdlib>
-////#include <iostream>
-////#include <memory>
-////#include <utility>
-////#include <boost/asio.hpp>
-////#include <ctime>
-////#include <string>
-////#include <boost/bind.hpp>
-////#include <boost/shared_ptr.hpp>
-////#include <boost/enable_shared_from_this.hpp>
-////#include <boost/asio.hpp>
-////
-////struct DataPacket{
-////    unsigned long length;
-////    const char *  data;
-////
-////    DataPacket(unsigned long l, const char * d) : length(l), data(d) {}
-////};
-////
-////class BasicMessage{
-////    unsigned char op;
-////    DataPacket    packet;
-////};
+//// Created by Ragnarr Ivarssen on 09.07.18.
 ////
 //
-#include "serializeable.h"
-//#include <string>
-//#include <vector>
-//#include <set>
+#ifndef RIVERCOIN_CPP_NETWORK_H
+#define RIVERCOIN_CPP_NETWORK_H
 //
-//#include <boost/asio.hpp>
-//#include <boost/array.hpp>
+//#include <cstdlib>
 //#include <iostream>
+//#include <memory>
+//#include <utility>
+//#include <boost/asio.hpp>
+//#include <ctime>
+//#include <string>
+//#include <boost/bind.hpp>
+//#include <boost/shared_ptr.hpp>
+//#include <boost/enable_shared_from_this.hpp>
+//#include <boost/asio.hpp>
 //
-//#define MSG_RECEIVED_SUCCESSFULLY 0
-//#define MSG_CORRUPTED 1
-//#define MSG_BLOCK_SOLUTION 2
-//#define MSG_FULL_BLOCK_GET 3
-//#define MSG_TRANSACTION 4
-//#define MSG_BLOCKCHAIN_SIZE 5
-//#define MSG_GREET 6
-//#define MSG_GREET_BACK 7
-//#define MSG_GOODBYE 8
-//#define MSG_FULL_BLOCK_SET 9
+//struct DataPacket{
+//    unsigned long length;
+//    const char *  data;
 //
+//    DataPacket(unsigned long l, const char * d) : length(l), data(d) {}
+//};
+//
+//class BasicMessage{
+//    unsigned char op;
+//    DataPacket    packet;
+//};
+//
+
+#include "serializeable.h"
+#include <string>
+#include <vector>
+#include <set>
+
+#define MSG_RECEIVED_SUCCESSFULLY 0
+#define MSG_CORRUPTED 1
+#define MSG_BLOCK_SOLUTION 2
+#define MSG_FULL_BLOCK_GET 3
+#define MSG_TRANSACTION 4
+#define MSG_BLOCKCHAIN_SIZE 5
+#define MSG_GREET 6
+#define MSG_GREET_BACK 7
+#define MSG_GOODBYE 8
+#define MSG_FULL_BLOCK_SET 9
+
 typedef serializeable<char*> Message;
-//class Context;
-//
-///** this is a container for all the TCP socket code **/
-//struct SocketConnection{
-//    const std::string               ip;
-//    const unsigned short            port;
-//    bool                            connected;
-//    boost::asio::io_service         ios;
-//    boost::asio::ip::tcp::socket    socket;
-//    std::vector<Message>            messages;
-//    SocketConnection(const std::string _ip_, unsigned short _port_);// : ip(_ip_), port(_port_) {}
-//    void makeConnection();
-//    bool isConnected();
-//    void disconnect();
-//    void send(const std::string& msg, boost::system::error_code& error);
-//    void receive();
-//};
-//
-///** this class should handle a single TCPConnectionSocket to send and receive high level data (Messages) **/
-//class Client{
-//private:
-//    SocketConnection connection;
-//    static Message interpretMessage(unsigned char msg);
-//public:
-//    void send(serializeable<char*> msg);
-//    void receive(Context* context);
-//    void connect();
-//    void disconnect();
-//    void execute();
-//};
-//
-///** this class should handle multiple clients by sending and receiving data to and from the client set **/
-//class Server{
-//private:
-//    std::set<Client>        clients;
-//    std::set<std::string>   ipAddresses;
-//    Context*                context;
-//    void makeConnection(const std::string& ip);
-//    void acceptConnections();
-//    void readAllIpAddresses();
-//    void writeAllIpAddresses();
-//public:
-//    Server(Context* context);
-//    void send(Message msg);
-//    void close();
-//};
-//
-//#endif //RIVERCOIN_CPP_NETWORK_H
+class Context;
+class Socket;
+class ServerSocket;
+
+/** this is a container for all the TCP socket code **/
+struct SocketConnection{
+    const std::string               ip;
+    const unsigned short            port;
+    bool                            connected;
+    Socket*                         socket;
+    std::vector<Message>            messages;
+    SocketConnection(const std::string _ip_, unsigned short _port_);// : ip(_ip_), port(_port_) {}
+    void makeConnection();
+    bool isConnected();
+    void disconnect();
+    void send(const std::string& msg);
+    void receive();
+};
+
+/** this class should handle a single TCPConnectionSocket to send and receive high level data (Messages) **/
+class Client{
+private:
+    SocketConnection connection;
+    static Message interpretMessage(unsigned char msg);
+public:
+    void send(serializeable<char*> msg);
+    void receive(Context* context);
+    void connect();
+    void disconnect();
+    void execute();
+};
+
+/** this class should handle multiple clients by sending and receiving data to and from the client set **/
+class Server{
+private:
+    std::set<Client>        clients;
+    std::set<std::string>   ipAddresses;
+    Context*                context;
+    void makeConnection(const std::string& ip);
+    void acceptConnections();
+    void readAllIpAddresses();
+    void writeAllIpAddresses();
+public:
+    Server(Context* context);
+    void send(Message msg);
+    void close();
+};
+
+#endif //RIVERCOIN_CPP_NETWORK_H
