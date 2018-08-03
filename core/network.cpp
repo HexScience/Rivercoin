f4bd93b616bc862cc7e14804dd77a2c6856bd20e
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-08-03 07:23:51 +0200

    brought networking back in the game!!!. . ,.daodlajmdlwd

--- core/network.cpp
+++ core/network.cpp
@@ -1,66 +1,66 @@
-////
-//// Created by Ragnarr Ivarssen on 09.07.18.
-////
 //
-//#include "network.h"
-//#include "context.h"
-//#include "config.h"
+// Created by Ragnarr Ivarssen on 09.07.18.
 //
-//#include <boost/asio.hpp>
-//#include <boost/array.hpp>
-//#include <iostream>
+
+#include "network.h"
+#include "context.h"
+#include "config.h"
+
+#include <boost/asio.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+
+#ifndef SOCKETS_
+#define SOCKETS_
+
+struct Socket{
+    boost::asio::io_service                 ios;
+    boost::asio::ip::tcp::socket            socket;
+
+    Socket() : socket(ios) {}
+//    Socket(const Socket& o) : socket(o.socket) { ios = socket.get_io_context(); }
+};
+
+struct ServerSocket{
+};
+
+#endif
+
+SocketConnection::SocketConnection(const std::string _ip_, unsigned short _port_) : ip(_ip_), port(_port_)
+{
+}
+
+void SocketConnection::makeConnection()
+{
+//    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
 //
-//#ifndef SOCKETS_
-//#define SOCKETS_
+//    socket->socket.connect(endpoint);
+}
+
+bool SocketConnection::isConnected()
+{
+//    return socket->socket.is_open();
+    return false;
+}
+
+void SocketConnection::disconnect()
+{
+//    socket->socket.close();
+}
+
+void SocketConnection::send(const std::string& msg)
+{
+//    boost::array<char, 4> buf;
+//    std::copy(msg.begin(), msg.end(), buf);
 //
-//struct Socket{
-//    boost::asio::io_service                 ios;
-//    boost::asio::ip::tcp::socket            socket;
+//    boost::system::error_code e;
 //
-//    Socket() : socket(ios) {}
-////    Socket(const Socket& o) : socket(o.socket) { ios = socket.get_io_context(); }
-//};
-//
-//struct ServerSocket{
-//};
-//
-//#endif
-//
-//SocketConnection::SocketConnection(const std::string _ip_, unsigned short _port_) : ip(_ip_), port(_port_)
-//{
-//}
-//
-//void SocketConnection::makeConnection()
-//{
-////    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string(ip), port);
-////
-////    socket->socket.connect(endpoint);
-//}
-//
-//bool SocketConnection::isConnected()
-//{
-////    return socket->socket.is_open();
-//    return false;
-//}
-//
-//void SocketConnection::disconnect()
-//{
-////    socket->socket.close();
-//}
-//
-//void SocketConnection::send(const std::string& msg)
-//{
-////    boost::array<char, 4> buf;
-////    std::copy(msg.begin(), msg.end(), buf);
-////
-////    boost::system::error_code e;
-////
-////    socket->socket.async_write_some(boost::asio::buffer(buf, msg.size()), e);
-//}
-//
-//void SocketConnection::receive()
-//{
-////    socket->socket.read_some(out);
-//}
-//
-//
+//    socket->socket.async_write_some(boost::asio::buffer(buf, msg.size()), e);
+}
+
+void SocketConnection::receive()
+{
+//    socket->socket.read_some(out);
+}
+
+
