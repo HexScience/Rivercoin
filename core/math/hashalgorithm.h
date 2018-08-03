474cee6652c265e77210a9547c7cb9c9d506ed24
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-08-03 06:59:30 +0200

    .

--- core/math/hashalgorithm.h
+++ core/math/hashalgorithm.h
@@ -14,26 +14,26 @@
 class HashAlgorithm {
 private:
 public:
-    template <unsigned int L> std::string base32(const Array<T, L>& a);
-    template <unsigned int L> std::string base58(const Array<T, L>& a);
+    template <unsigned int L> std::string base32(const Array<char, L>& a);
+    template <unsigned int L> std::string base58(const Array<char, L>& a);
 };
 
 namespace algorithms{
     namespace sha256{
-        static template <unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<char, L>& a);
+        static template <unsigned int L> std::string base58(const Array<char, L>& a);
     };
     namespace sha512{
-        static template <unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<char, L>& a);
+        static template <unsigned int L> std::string base58(const Array<char, L>& a);
     };
     namespace ripemd160{
-        static template <unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<char, L>& a);
+        static template <unsigned int L> std::string base58(const Array<char, L>& a);
     }
 
-    static template <unsigned int L> std::string base32(const Array<T, L>& a);
-    static template <unsigned int L> std::string base58(const Array<T, L>& a);
+    static template <unsigned int L> std::string base32(const Array<char, L>& a);
+    static template <unsigned int L> std::string base58(const Array<char, L>& a);
 }
 
 
