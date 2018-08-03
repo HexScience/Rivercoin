4137244091bf89eae89f315b2be14663468cef13
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-08-03 06:57:44 +0200

    only size should be argued in template.

--- core/math/hashalgorithm.h
+++ core/math/hashalgorithm.h
@@ -14,26 +14,26 @@
 class HashAlgorithm {
 private:
 public:
-    template <typename T, unsigned int L> std::string base32(const Array<T, L>& a);
-    template <typename T, unsigned int L> std::string base58(const Array<T, L>& a);
+    template <unsigned int L> std::string base32(const Array<T, L>& a);
+    template <unsigned int L> std::string base58(const Array<T, L>& a);
 };
 
 namespace algorithms{
     namespace sha256{
-        static template <typename T, unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <typename T, unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<T, L>& a);
+        static template <unsigned int L> std::string base58(const Array<T, L>& a);
     };
     namespace sha512{
-        static template <typename T, unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <typename T, unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<T, L>& a);
+        static template <unsigned int L> std::string base58(const Array<T, L>& a);
     };
     namespace ripemd160{
-        static template <typename T, unsigned int L> std::string base32(const Array<T, L>& a);
-        static template <typename T, unsigned int L> std::string base58(const Array<T, L>& a);
+        static template <unsigned int L> std::string base32(const Array<T, L>& a);
+        static template <unsigned int L> std::string base58(const Array<T, L>& a);
     }
 
-    static template <typename T, unsigned int L> std::string base32(const Array<T, L>& a);
-    static template <typename T, unsigned int L> std::string base58(const Array<T, L>& a);
+    static template <unsigned int L> std::string base32(const Array<T, L>& a);
+    static template <unsigned int L> std::string base58(const Array<T, L>& a);
 }
 
 
