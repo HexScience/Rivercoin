b08f66a167aa870656259548a5c55e2eb8d3cd35
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-02 16:21:30 +0100

    forkimpl

--- src/main/java/nucleus/protocols/ForkManager.java
+++ src/main/java/nucleus/protocols/ForkManager.java
@@ -1,5 +1,20 @@
 package nucleus.protocols;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class ForkManager
 {
+    private Set<ForkI> forks;
+
+    public ForkManager()
+    {
+        this.forks = new LinkedHashSet<>();
+    }
+
+    public void add(DownloadedBlock block)
+    {
+        for (ForkI fork : forks)
+            fork.add(block);
+    }
 }
