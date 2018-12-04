d626d707649b640ea15f2b2dc2923df397dfec1c
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-04 06:13:33 +0100

    miner finishes (so far).

--- src/main/java/nucleus/mining/FBO.java
+++ src/main/java/nucleus/mining/FBO.java
@@ -61,7 +61,7 @@ public class FBO
     public void bind()
     {
         glBindFramebuffer(GL_FRAMEBUFFER, fbo);
-        glViewport(0,0, width/5, height/5);
+        glViewport(0,0, width, height);
     }
 
     public void unbind()
