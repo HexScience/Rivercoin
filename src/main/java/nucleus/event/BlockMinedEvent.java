da6411024491be94b9cf8131f84c821f5c048f98
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-04 15:22:28 +0100

    Changed event type.

--- src/main/java/nucleus/event/BlockMinedEvent.java
+++ src/main/java/nucleus/event/BlockMinedEvent.java
@@ -4,8 +4,8 @@ import nucleus.protocols.protobufs.Block;
 
 public class BlockMinedEvent extends ActionableEvent<Block>
 {
-    public BlockMinedEvent(EventType type, long time, Block data)
+    public BlockMinedEvent(long time, Block data)
     {
-        super(type, time, data);
+        super(EventType.INTERNAL_NOTIFICATION, time, data);
     }
 }
