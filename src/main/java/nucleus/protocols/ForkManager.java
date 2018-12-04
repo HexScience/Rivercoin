47b5b3d94806cdc4bd48a2e913dca1bf4801b216
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-04 16:56:24 +0100

    forkmanager should handle forks semi decently (more code changes are due very soon ater I take a little nap)

--- src/main/java/nucleus/protocols/ForkManager.java
+++ src/main/java/nucleus/protocols/ForkManager.java
@@ -7,7 +7,8 @@ import java.util.Set;
 
 public class ForkManager
 {
-    private Set<ForkI> forks;
+    private Set<ForkI>  forks;
+    private ForkI       favourable;
 
     public ForkManager()
     {
@@ -23,6 +24,8 @@ public class ForkManager
         ForkI fork = null;
         forks.add(fork = new ForeignFork());
         fork.add(block);
+
+        checkShouldAdd(block.getBlock());
     }
 
     public void add(Block block)
@@ -34,5 +37,27 @@ public class ForkManager
         ForkI fork = null;
         forks.add(fork = new ForeignFork());
         fork.add(block);
+
+        checkShouldAdd(block);
+    }
+
+    private void checkShouldAdd(Block block)
+    {
+        Block existing      = favourable.getAt(block.getHeader().getBlockID());
+        boolean blockExists = existing != null;
+
+        if (!blockExists)
+            favourable.add(block);
+        else{
+            Block preexstng = favourable.getAt(block.getHeader().getBlockID());
+            long eTimeAvge  = existing.getHeader().getTimeStamp() - preexstng.getHeader().getTimeStamp();
+            long nTimeAvge  = block.getHeader().getTimeStamp() - preexstng.getHeader().getTimeStamp();
+
+            double eRatio   = existing.ratio();
+            double nRatio   = block.ratio();
+
+            if (!(eTimeAvge < nTimeAvge && eRatio > nRatio && existing.getTotalValue() > block.getTotalValue()))
+                favourable.replace(block.getHeader().getBlockID(), block);
+        }
     }
 }
