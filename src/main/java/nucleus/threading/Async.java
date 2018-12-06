9b374114428ddd21cfaac837a5c1939ddad699bf
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 10:58:03 +0100

    implemented utility functions.

--- src/main/java/nucleus/threading/Async.java
+++ src/main/java/nucleus/threading/Async.java
@@ -3,6 +3,7 @@ package nucleus.threading;
 public abstract class Async<E extends ThreadedAccess & Runnable>
 {
     protected E asyncronousObject;
+    protected Thread    current;
 
     public <T> T get(String name)
     {
@@ -12,5 +13,16 @@ public abstract class Async<E extends ThreadedAccess & Runnable>
     public <T> T blockingGet(String name)
     {
         return asyncronousObject.blockingGet(name);
+    }
+
+    public void start()
+    {
+        current = new Thread(asyncronousObject);
+        current.start();
+    }
+
+    public void abort()
+    {
+        current.interrupt();
     }
 }
