eca6e2b8ead9186898128343269d3f35352b3982
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 11:28:49 +0100

    changed the messagequeue to a linkedhashmap.

--- src/main/java/nucleus/net/ServerManager.java
+++ src/main/java/nucleus/net/ServerManager.java
@@ -9,30 +9,32 @@ import nucleus.net.server.IpAddress;
 import nucleus.net.server.PeerGroupCommunicator;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Queue;
 
 public class ServerManager
 {
-    private MessageQueue            messageQueue;
-    private RoundtripQue            roundTripQue;
-    private PeerGroupCommunicator   server;
-    private DatabaseReader          lookupService;
-    private IpAddressList           ipList;
+    private MessageQueue                    messageQueue;
+    private Map<byte[], MessageRoundTrip>   roundTripQue;
+    private PeerGroupCommunicator           server;
+    private DatabaseReader                  lookupService;
+    private IpAddressList                   ipList;
 
-    private Queue<IpAddress>        addresses;
+    private Queue<IpAddress>                addresses;
 
     public ServerManager(FileService entryPoint) throws IOException, FileServiceException, GeoIp2Exception
     {
         this.messageQueue   = new MessageQueue();
-        this.roundTripQue   = new RoundtripQue();
+        this.roundTripQue   = new LinkedHashMap<>();
         this.lookupService  = new DatabaseReader.Builder(entryPoint.newFile("GeoLiteC").newFile("GeoLiteC.mmdb").file()).build();
         this.ipList         = new IpAddressList(entryPoint, lookupService);
     }
 
     public void sendMessage(Message message)
     {
-        if (message.getType() == Message.RSPND)
-            roundTripQue.push(new MessageRoundTrip(message));
+        roundTripQue.put(message.getCheckSum(), new MessageRoundTrip(message));
+        server.send(message);
     }
 
     public IpAddressList getIpList()
