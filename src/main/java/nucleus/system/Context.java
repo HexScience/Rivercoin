976d3733a12fb8c7ee3bc610884a334375335ee3
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 10:05:14 +0100

    version control

--- src/main/java/nucleus/system/Context.java
+++ src/main/java/nucleus/system/Context.java
@@ -3,6 +3,7 @@ package nucleus.system;
 import nucleus.event.EventManager;
 import nucleus.ledger.Ledger;
 import nucleus.net.ServerManager;
+import nucleus.versioncontrol.VersionControl;
 import org.iq80.leveldb.DB;
 
 public interface Context
@@ -13,6 +14,7 @@ public interface Context
     Ledger getLedger();
     DB getDB();
     EventManager getEventManager();
+    VersionControl getVersionControl();
 
     boolean keepAlive();
 }
