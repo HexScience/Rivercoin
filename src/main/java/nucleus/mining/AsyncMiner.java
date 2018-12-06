285994c9258c707e0446ad9e6e6d9bc7afb77e01
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 10:57:34 +0100

    added more threading utility functions.

--- src/main/java/nucleus/mining/AsyncMiner.java
+++ src/main/java/nucleus/mining/AsyncMiner.java
@@ -6,6 +6,8 @@ public class AsyncMiner extends Async<MiningThread>
 {
     public void setMinerInstance(MiningThread thread)
     {
+        if (asyncronousObject != null)
+            abort();
         this.asyncronousObject = thread;
     }
 }
