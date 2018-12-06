d009a573be3a7658cf28a69de3b8cf92e857e34f
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 16:56:24 +0100

    implementing message class changes.

---
+++ src/main/java/nucleus/net/protocol/message/PingMessage.java
@@ -0,0 +1,33 @@
+package nucleus.net.protocol.message;
+
+import nucleus.net.protocol.Message;
+import nucleus.net.protocol.RequestMessage;
+import nucleus.system.Context;
+import nucleus.util.ByteUtil;
+
+/**
+ * This class should be used when notifying peers of the servers presence.
+ */
+public class PingMessage extends RequestMessage
+{
+    public PingMessage(long chainHeight)
+    {
+        super(PING, ByteUtil.encode(chainHeight));
+    }
+
+    /**
+     * @return null as this is a notification message.
+     * @param context
+     */
+    @Override
+    public Message getAnswerMessage(Context context)
+    {
+        return new PongMessage(context.getBlockChain().chainSize());
+    }
+
+    @Override
+    public String toString()
+    {
+        return null;
+    }
+}
