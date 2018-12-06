52657c7183ddd2ff14bee233e5e181a118422195
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-06 16:55:08 +0100

    new event listener to match the new event.

---
+++ src/main/java/nucleus/event/PeerDisconnectEventListener.java
@@ -0,0 +1,9 @@
+package nucleus.event;
+
+public interface PeerDisconnectEventListener extends EventListener<PeerDisconnectEvent>
+{
+    default EventType getType()
+    {
+        return EventType.NOTIFICATION;
+    }
+}
