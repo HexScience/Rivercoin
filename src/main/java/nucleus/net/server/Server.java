1b885228769ffd37c04e39cd08595eb3c0fd898e
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-07 12:03:46 +0100

    message decoding.

--- src/main/java/nucleus/net/server/Server.java
+++ src/main/java/nucleus/net/server/Server.java
@@ -1,10 +1,12 @@
 package nucleus.net.server;
 
+import nucleus.consensys.DownloadedBlock;
+import nucleus.event.BlockNotificationEvent;
 import nucleus.net.ServerManager;
 import nucleus.net.p2p.udp.PacketSendReceive;
 import nucleus.net.protocol.Message;
-import nucleus.net.protocol.message.CorruptedMessage;
-import nucleus.net.protocol.message.PingMessage;
+import nucleus.net.protocol.message.*;
+import nucleus.protocols.protobufs.Block;
 import nucleus.system.Context;
 import nucleus.system.Parameters;
 import nucleus.util.ByteUtil;
@@ -15,6 +17,8 @@ import java.io.IOException;
 import java.net.*;
 import java.util.*;
 import java.util.concurrent.LinkedBlockingDeque;
+
+import static nucleus.util.HashUtil.applySha256;
 
 public class Server
 {
@@ -40,13 +44,17 @@ public class Server
      */
     private void broadcast()
     {
+        int numAlready = connected.size();
         for (IpAddress ipAddress : context.getServerManager().getIpList().get())
         {
             try
             {
                 connectTo(ipAddress);
 
-                if (connected.size() > 60)
+                /**
+                 * check that at least 120 new connections were made
+                 */
+                if ((connected.size() - numAlready) > 120)
                     return;
             } catch (UnknownHostException e)
             {
@@ -99,6 +107,11 @@ public class Server
             if (System.currentTimeMillis() - lastreconnect > 35_000L)
                 reconnect();
 
+            removeInactive();
+
+            if (getNumActivePeers() < 20)
+                broadcast();
+
             byte header[] = new byte[(int) Message.HEADER_SIZE];
             DatagramPacket packet = new DatagramPacket(header, header.length);
 
@@ -113,11 +126,100 @@ public class Server
                 byte checksum[] = ByteUtil.trim(header_in, 2, 34);
                 int  size = ByteUtil.decodei(ByteUtil.trim(header_in, 34, 38));
 
+                if (packet.getLength() != Message.HEADER_SIZE)
+                    throw new IOException("header size incompatible.");
+
                 byte message[] = new byte[size];
 
                 try{
                     DatagramPacket msgBody = new DatagramPacket(message, message.length);
                     socket.receive(msgBody);
+
+                    message = msgBody.getData();
+
+                    if (msgBody.getLength() != size)
+                        throw new IOException("message body size invalid.");
+                    if (!ByteUtil.equals(applySha256(applySha256(message)), checksum))
+                        throw new IOException("checksum invalid.");
+
+
+                    if (type == Message.NOTFY)
+                    {
+                        switch (code)
+                        {
+                            case Message.PUNCH:
+                                break;
+                            case Message.PING:
+                                send(new PongMessage(context.getBlockChain().chainSize()), new IpAddress(msgBody.getAddress()));
+                                break;
+                            case Message.PONG:
+                                setPeerChainSize(new IpAddress(msgBody.getAddress()), ByteUtil.decode(message));
+                                break;
+                            case Message.BLOCK:
+                                exclude_send(new BlockNotifyMessage(message), IpAddress.pack(new IpAddress(msgBody.getAddress())));
+                                BlockNotificationEvent event = new BlockNotificationEvent(System.currentTimeMillis(), new DownloadedBlock(new Block(message), new IpAddress(msgBody.getAddress())));
+
+                                context.getEventManager().async_fire(event, "Block");
+                                break;
+                            case Message.BLOCKHEADER:
+                            case Message.CHAIN:
+                            case Message.CHAINSIZE:
+                            case Message.TRANSACTION:
+                            case Message.MSG_CORRUPTED:
+                                send(new CorruptedMessage(message).getAnswerMessage(context, context.getServerManager()), msgBody.getAddress());
+                                break;
+                            case Message.PEERS:
+                                removeInactive();
+                                if (getNumActivePeers() > 0)
+                                    send(new PeerNotificationMessage(connected));
+
+                                Set<IpAddress> decoded = PeerNotificationMessage.decode(message);
+
+                                for (IpAddress peer : decoded)
+                                    connectTo(peer);
+                                break;
+                            default:
+                                break;
+                        }
+                    } else if (type == Message.REPLY)
+                    {
+                        switch (code)
+                        {
+                            case Message.PUNCH:
+                                break;
+                            case Message.PING:
+                            case Message.PONG:
+                            case Message.BLOCK:
+                                exclude_send(new BlockNotifyMessage(message), IpAddress.pack(new IpAddress(msgBody.getAddress())));
+                                BlockNotificationEvent event = new BlockNotificationEvent(System.currentTimeMillis(), new DownloadedBlock(new Block(message), new IpAddress(msgBody.getAddress())));
+
+                                context.getEventManager().async_fire(event, "Block");
+                                break;
+                            case Message.BLOCKHEADER:
+                            case Message.CHAIN:
+                            case Message.CHAINSIZE:
+                            case Message.TRANSACTION:
+                            case Message.MSG_CORRUPTED:
+                                send(new CorruptedMessage(message).getAnswerMessage(context, context.getServerManager()), msgBody.getAddress());
+                                break;
+                            case Message.PEERS:
+                                removeInactive();
+                                if (getNumActivePeers() > 0)
+                                    send(new PeerNotificationMessage(connected));
+
+                                Set<IpAddress> decoded = PeerNotificationMessage.decode(message);
+
+                                for (IpAddress peer : decoded)
+                                    connectTo(peer);
+                                break;
+                            default:
+                                break;
+                        }
+                    } else if (type == Message.OPTIN)
+                    {
+                    } else if (type == Message.REQUEST)
+                    {
+                    }
                 } catch (IOException e)
                 {
                     InetAddress peer = packet.getAddress();
@@ -128,6 +230,36 @@ public class Server
             {
             }
         }
+    }
+
+    private void setPeerChainSize(IpAddress address, long decode)
+    {
+        for (IpAddress peer : connected)
+            if (peer.equals(address))
+                peer.setChainSize(decode);
+    }
+
+    private long getNumActivePeers()
+    {
+        long num = 0;
+
+        for (IpAddress address : connected)
+            num += address.isActive() ? 1 : 0;
+
+        return num;
+    }
+
+    private void removeInactive()
+    {
+//        Set<IpAddress> inactive = new LinkedHashSet<>();
+//
+//        for (IpAddress address : connected)
+//            if (!address.isActive()) inactive.add(address);
+////            else if (address.isActive() && disconnected.contains(address))
+////                disconnected.remove(address);
+//
+//        connected.removeAll(inactive);
+//        disconnected.addAll(inactive);
     }
 
     private static DatagramPacket createPacket(InetAddress ip, short port, Message message)
@@ -146,6 +278,30 @@ public class Server
             send(message, address);
     }
 
+    /**
+     * @param message The message to broadcast.
+     * @param exclude An array of excluded ip addresses,
+     *                they must not be null or empty.
+     * @throws IOException An exception if the
+     */
+    public void exclude_send(Message message, IpAddress[] exclude)
+    {
+        if (message == null) return;
+
+        for (IpAddress address : connected)
+            if (!contains(exclude, address))
+                send(message, address);
+    }
+
+    private boolean contains(IpAddress[] array, IpAddress check)
+    {
+        for (IpAddress address : array)
+            if (address.equals(check))
+                return true;
+
+        return false;
+    }
+
     public void connectTo(IpAddress address) throws IOException
     {
         socket.send(createPacket(address.getAddress(), Parameters.MAIN_NETWORK_NODE_PORT,  new PingMessage(context.getBlockChain().chainSize())));
@@ -161,11 +317,15 @@ public class Server
      */
     public void send(Message message, IpAddress peer)
     {
+        if (message == null) return;
+
         this.send(message, peer.getAddress());
     }
 
     public void send(Message message, InetAddress peer)
     {
+        if (message == null) return;
+
         long sent = 0;
 
         try {
