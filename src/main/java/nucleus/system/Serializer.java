5d868e3470632984a5b9eab127261ae34b290576
Ragnarr Ivarssen <Riverssen@gmail.com>
2018-12-07 12:04:14 +0100

    serializer constructor.

--- src/main/java/nucleus/system/Serializer.java
+++ src/main/java/nucleus/system/Serializer.java
@@ -1,17 +1,29 @@
 package nucleus.system;
 
+import nucleus.exceptions.FileServiceException;
 import nucleus.protocols.protobufs.Block;
 import nucleus.protocols.protobufs.BlockHeader;
 import nucleus.protocols.transaction.Transaction;
 import nucleus.protocols.transaction.TransactionOutput;
+import nucleus.util.FileService;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Serializer
 {
-    public Block loadBlock(long block)
+    private Context     context;
+    private FileService entryPoint;
+
+    public Serializer(Context context, FileService entryPoint)
     {
-        return null;
+        this.context    = context;
+        this.entryPoint = entryPoint;
+    }
+
+    public Block loadBlock(long block) throws IOException, FileServiceException
+    {
+        return new Block(entryPoint.newFile("height_" + block + ".blk"));
     }
 
     public BlockHeader loadHeader(long block)
@@ -19,12 +31,12 @@ public class Serializer
         return null;
     }
 
-    public Transaction loadTransaction(long block, int transactionID)
+    public Transaction loadTransaction(long block, int transactionID) throws IOException, FileServiceException
     {
         return new ArrayList<Transaction>(loadBlock(block).getAcceptedTransactions()).get(transactionID);
     }
 
-    public TransactionOutput loadTransactionOutput(long block, int transaction, int id)
+    public TransactionOutput loadTransactionOutput(long block, int transaction, int id) throws IOException, FileServiceException
     {
         return loadTransaction(block, transaction).getOutput(id);
     }
